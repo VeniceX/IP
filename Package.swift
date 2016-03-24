e5398ba373f8f85656d7b453df7b6b91a3ebc889
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-24 20:33:08 -0300

    rename system to os

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "IP",
     dependencies: [
         .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/System.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4)
     ]
 )
--- Source/IPError.swift
+++ Source/IPError.swift
@@ -22,7 +22,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-@_exported import System
+@_exported import OS
 
 public enum IPError: ErrorProtocol {
     case invalidPort(description: String)

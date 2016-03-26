1a132f40e56fc225e7b2e9ce784bf17b406dc65a
Paulo Faria <paulo.faria.rl@gmail.com>
2016-03-25 22:05:03 -0300

    move to VeniceX

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "IP",
     dependencies: [
-        .Package(url: "https://github.com/Zewo/Venice.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4)
+        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4),
     ]
 )

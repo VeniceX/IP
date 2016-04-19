bad5e16f0c9e68821c7daf36265aaf00abbebf9d
Scott Byrns <contact@scottbyrns.com>
2016-04-19 08:23:25 -0600

    Migrate to April 12, 2016 snapshot (#1)
    * Migrate to April 12, 2016 snapshot
    
    * use POSIX error
    
    * remove errorDescription

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ import PackageDescription
 let package = Package(
     name: "IP",
     dependencies: [
-        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 4),
-        .Package(url: "https://github.com/Zewo/OS.git", majorVersion: 0, minor: 4),
+        .Package(url: "https://github.com/VeniceX/Venice.git", majorVersion: 0, minor: 5),
+        .Package(url: "https://github.com/Zewo/POSIX.git", majorVersion: 0, minor: 5),
     ]
 )
--- Source/IP.swift
+++ Source/IP.swift
@@ -24,6 +24,19 @@
 
 import CLibvenice
 @_exported import Venice
+@_exported import POSIX
+
+public enum IPError: ErrorProtocol {
+    case invalidPort
+}
+
+extension IPError: CustomStringConvertible {
+    public var description: String {
+        switch self {
+        case .invalidPort: return "Port number should be between 0 and 0xffff"
+        }
+    }
+}
 
 public enum IPMode {
     case ipV4
@@ -44,33 +57,34 @@ public enum IPMode {
 public struct IP {
     public let address: ipaddr
 
-    public init(address: ipaddr) throws {
+    public init(address: ipaddr) {
         self.address = address
-        try IPError.assertNoError()
     }
 
-    public init(localAddress: String? = nil, port: Int = 0, mode: IPMode = .ipV4Prefered) throws {
-        try IP.assertValidPort(port)
-        if let localAddress = localAddress {
-            try self.init(address: iplocal(localAddress, Int32(port), mode.code))
-        } else {
-            try self.init(address: iplocal(nil, Int32(port), mode.code))
-        }
+    public init(port: Int = 0, mode: IPMode = .ipV4Prefered) throws {
+        try IP.assertValid(port)
+        let address = iplocal(nil, Int32(port), mode.code)
+        try ensureLastOperationSucceeded()
+        self.init(address: address)
     }
 
-    public init(networkInterface: String, port: Int = 0, mode: IPMode = .ipV4Prefered) throws {
-        try IP.assertValidPort(port)
-        try self.init(address: iplocal(networkInterface, Int32(port), mode.code))
+    public init(localAddress: String, port: Int = 0, mode: IPMode = .ipV4Prefered) throws {
+        try IP.assertValid(port)
+        let address = iplocal(localAddress, Int32(port), mode.code)
+        try ensureLastOperationSucceeded()
+        self.init(address: address)
     }
 
     public init(remoteAddress: String, port: Int, mode: IPMode = .ipV4Prefered, deadline: Double = .never) throws {
-        try IP.assertValidPort(port)
-        try self.init(address: ipremote(remoteAddress, Int32(port), mode.code, deadline.int64milliseconds))
+        try IP.assertValid(port)
+        let address = ipremote(remoteAddress, Int32(port), mode.code, deadline.int64milliseconds)
+        try ensureLastOperationSucceeded()
+        self.init(address: address)
     }
 
-    private static func assertValidPort(port: Int) throws {
+    private static func assertValid(_ port: Int) throws {
         if port < 0 || port > 0xffff {
-            throw IPError.invalidPort(description: "Port number should be between 0 and 0xffff")
+            throw IPError.invalidPort
         }
     }
 }
